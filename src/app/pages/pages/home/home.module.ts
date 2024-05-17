355f869814a5db418a052d9b02c1273c5cbf1300
Diego Juan Isaquiel <diegocirilo51@gmail.com>
2024-05-17 16:33:36 -0300

    feat(home): Home Page was created

--- src/app/pages/pages/home/home.module.ts
+++ src/app/pages/pages/home/home.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 //#endregion
 
@@ -12,6 +13,7 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
 @NgModule({
   imports: [
     CommonModule,
+    MatIconModule,
     RouterModule.forChild(routes)
   ],
   exports: [
